c484c84728aaa32b57f64ebf394334b32af716fe
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-04-12 23:16:15 +0300

    change class to className

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,23 +5,23 @@ export const Profile = ({ username, tag, location, avatar, stats }) => (
   <div className={css.profile}>
     <div class="description">
       <img src={avatar} alt="User avatar" class="avatar" />
-      <p class="name">{username}</p>
-      <p class="tag">@{tag}</p>
-      <p class="location">{location}</p>
+      <p className={css.name}>{username}</p>
+      <p className={css.tag}>@{tag}</p>
+      <p className={css.location}>{location}</p>
     </div>
 
-    <ul class="stats">
+    <ul className={css.stats}>
       <li>
-        <span class="label">Followers</span>
-        <span class="quantity">{stats.followers}</span>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{stats.followers}</span>
       </li>
       <li>
-        <span class="label">Views</span>
-        <span class="quantity">{stats.views}</span>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{stats.views}</span>
       </li>
       <li>
-        <span class="label">Likes</span>
-        <span class="quantity">{stats.likes}</span>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{stats.likes}</span>
       </li>
     </ul>
   </div>
