c5b6f2c55a6a321861d004326827c93c1cfd756e
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-04-14 22:26:08 +0300

    fix is to js

---
+++ src/components/transactionhistory/TransactionHistory.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+
+
+
+
+
+
+{"id": "c6e5784b-0ca3-48d6-86e5-b5128af5a523",
+
+    "type": "invoice",
+    "amount": "116.11",
+    "currency": "CUP CUC"
+}
