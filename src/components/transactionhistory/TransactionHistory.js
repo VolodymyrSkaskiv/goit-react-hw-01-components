1584e1daf66a045ce3d951d23d89cc455aec3cf8
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-04-14 22:56:05 +0300

    Update TransactionHistory.js

--- src/components/transactionhistory/TransactionHistory.js
+++ src/components/transactionhistory/TransactionHistory.js
@@ -1,40 +1,26 @@
 import PropTypes from 'prop-types';
 import css from './TransactionHistory.module.css';
 
-export const TransactionHistory = ({ transactions}) => {
-    <table class="transaction-history">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
+export const TransactionHistory = ({ transactions }) => {
+  <table class="transaction-history">
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
 
-  <tbody>
-    <tr>
-      <td>Invoice</td>
-      <td>125</td>
-      <td>USD</td>
-    </tr>
-    <tr>
-      <td>Withdrawal</td>
-      <td>85</td>
-      <td>USD</td>
-    </tr>
-  </tbody>
-</table>
- } ;
-
-
-
-
-
-
-
-{"id": "c6e5784b-0ca3-48d6-86e5-b5128af5a523",
-
-    "type": "invoice",
-    "amount": "116.11",
-    "currency": "CUP CUC"
-}
+    <tbody>
+      {transactions.map(({ id, type, amount, currency }) => {
+        return (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>;
+};
