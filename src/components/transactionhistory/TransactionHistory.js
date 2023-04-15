097fb7534b64aece70493a2237cf17c859abe1f1
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-04-15 20:10:11 +0300

    add css transactions

--- src/components/transactionhistory/TransactionHistory.js
+++ src/components/transactionhistory/TransactionHistory.js
@@ -3,22 +3,22 @@ import css from './TransactionHistory.module.css';
 
 export const TransactionHistory = ({ transactions }) => {
   return (
-    <table class="transaction-history">
-      <thead>
+    <table className={css.transactionHistory}>
+      <thead className={css.head}>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.th}>Type</th>
+          <th className={css.th}>Amount</th>
+          <th className={css.th}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {transactions.map(({ id, type, amount, currency }) => {
           return (
-            <tr key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
+            <tr key={id} className={css.tr}>
+              <td className={css.td}>{type}</td>
+              <td className={css.td}>{amount}</td>
+              <td className={css.td}>{currency}</td>
             </tr>
           );
         })}
