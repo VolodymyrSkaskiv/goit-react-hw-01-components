313e39b7d48b937abdaf58b4385ae654e86b339e
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-04-15 19:49:35 +0300

    add css friendlist

--- src/components/friendlist/FriendListItem.js
+++ src/components/friendlist/FriendListItem.js
@@ -3,13 +3,12 @@ import css from './friendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => (
   <li className={css.item}>
-    <img className={css.avatar} src={avatar} alt={name} />
-    <span className={css.name}>{name}</span>
-
     <span
       className={css.status}
       style={{ background: isOnline ? 'green' : 'red' }} //різний колір коли online,offline
     ></span>
+    <img className={css.avatar} src={avatar} alt={name} />
+    <span className={css.name}>{name}</span>
   </li>
 );
 
